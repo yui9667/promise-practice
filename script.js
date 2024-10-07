a519a5c083dd09ffcd3742b62e4c77a886977209
Yui Jensen <kyanyui67@gmail.com>
2024-10-07 14:37:10 +0200

    Replaced async/await from callbacks/promise

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
+//* This is week7 assignment
+
+// function myPromise() {
+//   const pokemonName = document
+//     .getElementById('pokemonName')
+//     .value.toLowerCase();
+//   //I wanted to not show imgElement before an user add name, but it seems does not work. Can you see what is the problem?
+//   const imgElement = document.getElementById('pokemonSprite');
+//   imgElement.style.display = 'none';
+//   if (!pokemonName) {
+//     alert('Please enter a pokemon name');
+//     return;
+//   }
+
+//   const fetchPokemon = new Promise((resolve, reject) => {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//       .then((response) => {
+//         if (!response.ok) {
+//           reject('Could not found!');
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         resolve(data);
+//       })
+//       .catch((error) => {
+//         reject(error);
+//       });
+//   });
+//   fetchPokemon
+//     .then((data) => {
+//       const pokemonSprite = data.sprites.front_default;
+
+//       imgElement.src = pokemonSprite;
+
+//       imgElement.style.display = 'block';
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+//*07/10/2024 Week 8: Asynchronous Programming: Async/Await
+
 function myPromise() {
   const pokemonName = document
     .getElementById('pokemonName')
     .value.toLowerCase();
-  //I wanted to not show imgElement before an user add name, but it seems does not work. Can you see what is the problem?
   const imgElement = document.getElementById('pokemonSprite');
-  imgElement.style.display = 'none';
+
   if (!pokemonName) {
     alert('Please enter a pokemon name');
     return;
   }
-
-  const fetchPokemon = new Promise((resolve, reject) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then((response) => {
-        if (!response.ok) {
-          reject('Could not found!');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-  fetchPokemon
-    .then((data) => {
+  imgElement.style.display = 'block';
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+      );
+      const data = await response.json();
       const pokemonSprite = data.sprites.front_default;
-
       imgElement.src = pokemonSprite;
-
-      imgElement.style.display = 'block';
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error(error);
-    });
+      alert('Could not found the pokemon😢Try again!');
+    }
+  };
+  fetchPokemon();
 }
